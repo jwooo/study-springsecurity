c5fc570611ef349c7c10c2144a4f0eb6ee5d717d
jwooo <parkjwoo0515@gmail.com>
2023-02-21 15:38:41 +0900

    스프링 시큐리티가 사용자를 관리하는 방법 지정

---
+++ chap03/ex2/src/main/java/com/example/ex2/config/ProjectConfig.java
@@ -0,0 +1,35 @@
+package com.example.ex2.config;
+
+import com.example.ex2.model.User;
+import com.example.ex2.service.InMemoryUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        String usersByUsernameQuery = "select username, password, enabled from users where username = ?";
+        String authsByUserQuery = "select username, authority from spring.authorities where username = ?";
+
+        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+        return userDetailsManager;
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
