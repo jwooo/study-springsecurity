f7ae95a773993d566016b1c880b2eeea1e56ba33
jwooo <parkjwoo0515@gmail.com>
2023-02-22 20:11:51 +0900

    실전 사용예제1

---
+++ chap06/ex1/src/main/java/com/example/ex1/entities/enums/Currency.java
@@ -0,0 +1,5 @@
+package com.example.ex1.entities.enums;
+
+public enum Currency {
+    USD, GBP, EUR
+}
---
+++ chap06/ex1/src/main/java/com/example/ex1/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.ex1.repositories;
+
+import com.example.ex1.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findUserByUsername(String u);
+}
