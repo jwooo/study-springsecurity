4cb405424b5d41a6ec133605dab54f9d436d28a6
jwooo <parkjwoo0515@gmail.com>
2023-02-24 17:48:28 +0900

    CSRF 보호 맞춤 구성

---
+++ chap10/ex3/src/main/java/com/example/ex3/repository/JpaTokenRepository.java
@@ -0,0 +1,11 @@
+package com.example.ex3.repository;
+
+import com.example.ex3.entity.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface JpaTokenRepository extends JpaRepository<Token, Integer> {
+
+    Optional<Token> findTokenByIdentifier(String identifier);
+}
