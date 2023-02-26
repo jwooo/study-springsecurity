5ec0686cdf2106dd4df1375259d1dfb6ee6fdf6c
jwooo <parkjwoo0515@gmail.com>
2023-02-26 21:07:20 +0900

    Remember Me 인증

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,9 +1,11 @@
 package io.security.basicsecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import javax.servlet.http.HttpSession;
 
@@ -11,41 +13,42 @@ import javax.servlet.http.HttpSession;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().authenticated();
-
-        http
+                .antMatchers("/loginPage").permitAll()
+                .anyRequest().authenticated()
+        .and()
                 .formLogin()
-                .loginPage("/login")
+                .loginProcessingUrl("/login")
                 .defaultSuccessUrl("/")
-                .failureUrl("/login")
+                .failureUrl("/login?error=true")
                 .usernameParameter("username")
                 .passwordParameter("password")
-                .loginProcessingUrl("login_proc")
                 .successHandler((request, response, authentication) -> {
-                    System.out.println("authentication" + authentication.getName());
+                    System.out.println("success");
                     response.sendRedirect("/");
                 })
-                .failureHandler((request, response, exception) -> {
-                    System.out.println("exception" + exception.getMessage());
+                .failureHandler((request, response, authentication) -> {
+                    System.out.println("failure");
                     response.sendRedirect("/login");
                 })
-                .permitAll();
-
-        http
+        .and()
                 .logout()
                 .logoutUrl("/logout")
-                .logoutSuccessUrl("/login")
-                .addLogoutHandler((request, response, authentication) -> {
-                    HttpSession session = request.getSession();
-                    session.invalidate();
-                })
+                .logoutSuccessUrl("login")
                 .logoutSuccessHandler((request, response, authentication) -> {
+                    System.out.println("logout");
                     response.sendRedirect("/login");
                 })
-                .deleteCookies("remember-me");
+        .and()
+                .rememberMe()
+                .rememberMeParameter("remember")
+                .tokenValiditySeconds(3600)
+                .userDetailsService(userDetailsService);
     }
 }
