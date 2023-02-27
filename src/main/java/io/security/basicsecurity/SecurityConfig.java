7b65f518a4d285ea3a46a77cb55b591977d4139d
jwooo <parkjwoo0515@gmail.com>
2023-02-27 15:39:44 +0900

    권한 설정과 표현식

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,11 +1,17 @@
 package io.security.basicsecurity;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 import javax.servlet.http.HttpSession;
 
@@ -13,42 +19,42 @@ import javax.servlet.http.HttpSession;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private UserDetailsService userDetailsService;
+    @Bean
+    UserDetailsManager users() {
+        UserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}1111")
+                .roles("USER")
+                .build();
+
+        UserDetails sys = User.builder()
+                .username("sys")
+                .password("{noop}1111")
+                .roles("SYS", "USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}1111")
+                .roles("ADMIN", "SYS", "USER")
+                .build();
+        manager.createUser(user);
+        manager.createUser(sys);
+        manager.createUser(admin);
+
+        return manager;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/loginPage").permitAll()
-                .anyRequest().authenticated()
-        .and()
-                .formLogin()
-                .loginProcessingUrl("/login")
-                .defaultSuccessUrl("/")
-                .failureUrl("/login?error=true")
-                .usernameParameter("username")
-                .passwordParameter("password")
-                .successHandler((request, response, authentication) -> {
-                    System.out.println("success");
-                    response.sendRedirect("/");
-                })
-                .failureHandler((request, response, authentication) -> {
-                    System.out.println("failure");
-                    response.sendRedirect("/login");
-                })
-        .and()
-                .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("login")
-                .logoutSuccessHandler((request, response, authentication) -> {
-                    System.out.println("logout");
-                    response.sendRedirect("/login");
-                })
-        .and()
-                .rememberMe()
-                .rememberMeParameter("remember")
-                .tokenValiditySeconds(3600)
-                .userDetailsService(userDetailsService);
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin/pay").hasRole("SYS")
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
+                .anyRequest().authenticated();
+        http.formLogin();
     }
 }
