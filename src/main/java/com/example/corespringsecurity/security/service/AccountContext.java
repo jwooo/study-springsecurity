b1cdbb56027a3bfcc52259b765f531ffe2bc8df8
jwooo <parkjwoo0515@gmail.com>
2023-03-01 18:22:37 +0900

    DB 연동 인증 처리: CustomUserDetailsService

---
+++ src/main/java/com/example/corespringsecurity/security/service/AccountContext.java
@@ -0,0 +1,18 @@
+package com.example.corespringsecurity.security.service;
+
+import com.example.corespringsecurity.domain.Account;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class AccountContext extends User {
+
+    private final Account account;
+
+    public AccountContext(Account account,Collection<? extends GrantedAuthority> authorities) {
+        super(account.getUsername(), account.getPassword(), authorities);
+        this.account = account;
+    }
+}
---
+++ src/main/java/com/example/corespringsecurity/security/service/CustomUserDetailsService.java
@@ -0,0 +1,39 @@
+package com.example.corespringsecurity.security.service;
+
+import com.example.corespringsecurity.domain.Account;
+import com.example.corespringsecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account account = userRepository.findByUsername(username);
+
+        if (account == null) {
+            throw new UsernameNotFoundException("Username Not Found Exception");
+        }
+
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new SimpleGrantedAuthority(account.getRole()));
+
+        AccountContext accountContext = new AccountContext(account, roles);
+
+        return accountContext;
+    }
+}
