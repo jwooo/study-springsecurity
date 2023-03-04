eadfa24c1912558b384f494d2fcef5c50c56fdd5
jwooo <parkjwoo0515@gmail.com>
2023-03-04 16:08:43 +0900

    spring security

--- src/main/java/io/getarryas/userservice/UserserviceApplication.java
+++ src/main/java/io/getarryas/userservice/UserserviceApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.management.MXBean;
 import java.util.ArrayList;
 
 @SpringBootApplication
@@ -15,6 +18,11 @@ public class UserserviceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserserviceApplication.class, args);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
