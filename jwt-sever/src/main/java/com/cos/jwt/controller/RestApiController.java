b8f89cb454906e1bf7f223e68f913deaacf142c1
jwooo <parkjwoo0515@gmail.com>
2023-03-09 22:11:34 +0900

    회원가입 로직

--- jwt-sever/src/main/java/com/cos/jwt/controller/RestApiController.java
+++ jwt-sever/src/main/java/com/cos/jwt/controller/RestApiController.java
@@ -1,12 +1,20 @@
 package com.cos.jwt.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.cos.jwt.model.User;
+import com.cos.jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class RestApiController {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
     @GetMapping("/home")
     public String home() {
@@ -16,6 +24,24 @@ public class RestApiController {
     @PostMapping("/token")
     public String token() {
         return "<h1>token</h1>";
+    }
+
+    @GetMapping("/manager/reports")
+    public String reports() {
+        return "<h1>reports</h1>";
+    }
+
+    @GetMapping("/admin/user")
+    public List<User> users() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping("/join")
+    public String join(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles("ROLE_USER");
+        userRepository.save(user);
+        return "회원가입 완료";
     }
 
 }
